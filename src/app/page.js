20f7321c6c4322fac181806ed879cdd197eac288
aguss rodriguez <agussrg80@gmail.com>
2023-05-10 19:47:34 -0300

    fix message

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,13 @@ export default function Home() {
 
 	return (
 		<section className="my-4 px-2 sm:px-0">
-			<div className="pb-4 text-xl">Cantidad de tareas: {tasks.length}</div>
+			<div className="pb-4 text-xl">
+				{tasks.length > 0 ? (
+					<span>Cantidad de tareas: {tasks.length} </span>
+				) : (
+					<span>No se han encontrado tareas</span>
+				)}
+			</div>
 			<div className="grid grid-cols-1 sm:grid-cols-container gap-4">
 				{tasks.map((task) => (
 					<TaskCard key={task.id} {...task} />
