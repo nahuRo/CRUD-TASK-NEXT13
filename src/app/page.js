2c30fadf155db8e8bf4e938ce839072822e786ae
aguss rodriguez <agussrg80@gmail.com>
2023-05-10 19:42:27 -0300

    add tasks length

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,13 @@ export default function Home() {
 	const { tasks } = useTasks();
 
 	return (
-		<section className="grid grid-cols-1 sm:grid-cols-container gap-4 my-4 px-2 sm:px-0">
-			{tasks.map((task) => (
-				<TaskCard key={task.id} {...task} />
-			))}
+		<section className="my-4 px-2 sm:px-0">
+			<div className="pb-4 text-xl">Cantidad de tareas: {tasks.length}</div>
+			<div className="grid grid-cols-1 sm:grid-cols-container gap-4">
+				{tasks.map((task) => (
+					<TaskCard key={task.id} {...task} />
+				))}
+			</div>
 		</section>
 	);
 }
